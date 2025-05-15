600a32b56670d15ab3725a8b92d0ec8899d8c642
w4z3d <wazed.wzd@gmail.com>
2025-05-15 16:31:21 +0200

    Add architecture base

---
+++ src/codegen/arch/x86/arch_x86.hpp
@@ -0,0 +1,7 @@
+#ifndef CODEGEN_ARCH_X86_ARCH_X86_H
+#define CODEGEN_ARCH_X86_ARCH_X86_H
+#include "../arch.hpp"
+
+class X86Architecture : public Architecture {};
+
+#endif // !CODEGEN_ARCH_X86_ARCH_X86_H
